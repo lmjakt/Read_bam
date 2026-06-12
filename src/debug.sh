f78d787c4ddbab626f832873a38a264774874088
Martin Jakt <lmjakt@gmail.com>
2026-06-12 14:56:56 +0200

    Removed compilation of kstring.c from src/debug.sh
    The code no longer relies on a local copy of kstring, instead
    using the modified version provided by htslib.
    Eventually the kstring files should also be removed.

--- src/debug.sh
+++ src/debug.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 ## run this to compile without optimisation.
+## local copy of kstring not needed as the code now depends on
+## the one in htslib
 gcc -I"/home/lmj/R/R-4.4.1/include" -DNDEBUG   -I/usr/local/include -Wall -fpic  -g -Og  -c read_bam.c -o read_bam.o
 gcc -I"/home/lmj/R/R-4.4.1/include" -DNDEBUG   -I/usr/local/include -Wall -fpic  -g -Og  -c common.c -o common.o
 gcc -I"/home/lmj/R/R-4.4.1/include" -DNDEBUG   -I/usr/local/include -Wall -fpic  -g -Og  -c qname_hash.c -o qname_hash.o
-gcc -I"/home/lmj/R/R-4.4.1/include" -DNDEBUG   -I/usr/local/include -Wall -fpic  -g -Og  -c kstring.c -o kstring.o
+## gcc -I"/home/lmj/R/R-4.4.1/include" -DNDEBUG   -I/usr/local/include -Wall -fpic  -g -Og  -c kstring.c -o kstring.o
 gcc -I"/home/lmj/R/R-4.4.1/include" -DNDEBUG   -I/usr/local/include -Wall -fpic  -g -Og  -c hiC.c -o hiC.o
 gcc -shared -L/usr/local/lib -o read_bam.so read_bam.o common.o qname_hash.o hiC.o -lhts
 
